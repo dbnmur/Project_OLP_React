f16fe1d03b6e851b5116032204d8e389fe7f20c8
Tomas Rimkus <rimk.tom@gmail.com>
2018-03-28 11:49:41 +0300

    Add float button in dashboard and instructor functionality

--- src/containers/Dashboard/index.js
+++ src/containers/Dashboard/index.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import Grid from 'material-ui/Grid';
+import Checkbox from 'material-ui/Checkbox';
+import { FormGroup, FormControlLabel } from 'material-ui/Form';
 
 import MyCard from './MyCard';
+import FloatButton from './FloatButton';
 
 const courses = [
+  {
+    title: 'Web Design',
+    instructor: 'Auksinis kardas',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+  },
   {
     title: 'Web Design',
     instructor: 'Auksinis kardas',
@@ -52,14 +61,33 @@ class Dashboard extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      courses: courses
+      courses: courses,
+      isInstructor: false
     };
   }
+
+  handleChange = name => event => {
+    this.setState({ [name]: event.target.checked });
+  };
+
   render() {
     const { courses } = this.state;
     return (
       <Grid container spacing={8} style={{ width: '100%', marginTop: '25px' }}>
+        <FormGroup row style={{ marginLeft: '45px' }}>
+          <FormControlLabel
+            control={
+              <Checkbox
+                checked={this.state.isInstructor}
+                onChange={this.handleChange('isInstructor')}
+                value="isInstructor"
+              />
+            }
+            label="Instructor"
+          />
+        </FormGroup>
         <Grid item md={12}>
+          {/* Grid for title */}
           <Grid
             container
             justify="center"
@@ -69,6 +97,7 @@ class Dashboard extends React.Component {
               <h1 style={{ marginBottom: '45px' }}>My courses</h1>
             </Grid>
           </Grid>
+          {/* Grid for courses */}
           <Grid
             container
             justify="center"
@@ -86,7 +115,19 @@ class Dashboard extends React.Component {
                 </Grid>
               );
             })}
+            {/* Grid for add button */}
           </Grid>
+          {this.state.isInstructor && (
+            <Grid
+              container
+              justify="flex-end"
+              alignItems="center"
+              style={{ marginTop: '25px' }}>
+              <Grid item>
+                <FloatButton />
+              </Grid>
+            </Grid>
+          )}
         </Grid>
       </Grid>
     );
